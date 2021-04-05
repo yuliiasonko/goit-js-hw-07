a34aa28fd1029f32b7478b2204d8f22d0e435c5b
yuliiasonko <yuliiasonko@gmail.com>
2021-04-05 19:18:06 +0300

    Create task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
+
+// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+const button = {
+  sub: document.querySelector("[data-action='increment']"),
+  add: document.querySelector("[data-action='decrement']"),
+  span: document.querySelector("#value"),
+};
+
+let counterValue = 0;
+const increment = () => {
+  counterValue += 1;
+
+ button.span.textContent = counterValue;
+};
+
+const decrement = () => {counterValue -= 1;
+
+  button.span.textContent = counterValue;
+};
+
+button.sub.addEventListener("click", increment);
+button.add.addEventListener("click", decrement);
